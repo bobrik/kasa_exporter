bb2dac43907ed0152b3cb9f0b7f20670fd3da00f
Ivan Babrou <github@ivan.computer>
2020-08-18 20:48:52 -0700

    Swap error-chain for thiserror+anyhow

--- src/kasa/error.rs
+++ src/kasa/error.rs
@@ -1,36 +1,18 @@
-use error_chain::error_chain;
+use thiserror::Error;
 
-error_chain! {
-    errors {
-        EmptyAuthResponse(code: i32, message: String) {
-            description("empty auth response")
-            display("empty auth response: code={} message={}", code, message)
-        }
-        EmptyPassthroughResponse {
-            description("empty passthrough response")
-            display("empty passthrough response")
-        }
-        EmptyEmeterResponse {
-            description("empty emeter response")
-            display("empty emeter response")
-        }
-        PassthtoughParams {
-            description("error initializing passthrough params")
-            display("error initializing passthrough params")
-        }
-        Deserialization(data: String) {
-            description("deserialization error")
-            display("error deserializing: {}", data)
-        }
-        Serialization(debug: String) {
-            description("serialization error")
-            display("error serializing: {}", debug)
-        }
-    }
-
-    foreign_links {
-        Hyper(hyper::error::Error);
-        Serde(serde_json::error::Error);
-        InvalidUri(http::uri::InvalidUri);
-    }
+#[derive(Error, Debug)]
+pub enum KasaError {
+    #[error("Empty auth response: code={code} message={message}")]
+    EmptyAuthResponse { code: i32, message: String },
+    #[error("Serialization error: debug={debug}")]
+    Serialization {
+        source: anyhow::Error,
+        debug: String,
+    },
+    #[error("Passthrough param initialization error")]
+    PassthtoughParams { source: anyhow::Error },
+    #[error("Empty passthrough response")]
+    EmptyPassthroughResponse {},
+    #[error("Empty emeter response")]
+    EmptyEmeterResponse {},
 }
